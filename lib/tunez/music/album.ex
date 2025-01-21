5c5f0c71b63c83f8a02c4d29269415e69a9aaa28
pikdum <pikdum@kuudere.moe>
2025-01-20 19:14:30 -0600

    feat: add unique index

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -60,5 +60,10 @@ defmodule Tunez.Music.Album do
     end
   end
 
+  identities do
+    identity :unique_album_names_per_artist, [:name, :artist_id],
+      message: "already exists for this artist"
+  end
+
   def next_year, do: Date.utc_today().year + 1
 end
