fc12e32521ce23b37c397e663fed198878e898db
pikdum <pikdum@kuudere.moe>
2025-01-21 18:14:05 -0600

    feat: add albums to graphql api

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -3,7 +3,11 @@ defmodule Tunez.Music.Album do
     otp_app: :tunez,
     domain: Tunez.Music,
     data_layer: AshPostgres.DataLayer,
-    extensions: [AshJsonApi.Resource]
+    extensions: [AshGraphql.Resource, AshJsonApi.Resource]
+
+  graphql do
+    type :album
+  end
 
   json_api do
     type "album"
@@ -76,6 +80,8 @@ defmodule Tunez.Music.Album do
     end
   end
 
+  def next_year, do: Date.utc_today().year + 1
+
   calculations do
     calculate :years_ago, :integer, expr(2025 - year_released)
 
@@ -83,8 +89,6 @@ defmodule Tunez.Music.Album do
               :string,
               expr("wow, this was released " <> years_ago <> " years ago!")
   end
-
-  def next_year, do: Date.utc_today().year + 1
 
   identities do
     identity :unique_album_names_per_artist, [:name, :artist_id],
